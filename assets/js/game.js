97b63944415cc3b39c64eb144b5f8ee588052271
Adam Smith <smith.adam937@gmail.com>
2022-05-12 12:35:03 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,10 +8,12 @@ var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyNames);
-console.log(enemyNames.length);
-console.log(enemyNames[0]);
-console.log(enemyNames[3]);
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min); 
+
+  return value; 
+}
+
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
@@ -28,14 +30,15 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack); 
+    enemyHealth = Math.max(0, enemyHealth - damage); 
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -54,7 +57,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -87,7 +91,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = Math.floor(Math.random() * 21) + 60;
 
     
 
@@ -109,6 +113,9 @@ var startGame = function() {
    }
   }
 }
+
+startGame(); 
+
 var shop = function() {
   var shopOption = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
   var shopOptionPrompt = shopOption.toLowerCase(); 
@@ -148,7 +155,9 @@ var shop = function() {
 
 }
 
-startGame(); 
+
+
+
 
   // function to end the entire game 
 var endGame = function() {
