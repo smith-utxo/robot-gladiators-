73db80befdac87c372c19fa237afbb619c1e1e16
Adam Smith <smith.adam937@gmail.com>
2022-05-13 12:03:24 -0500

    player name required fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,19 @@
+ 
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min); 
 
   return value; 
 }
 
+var getPlayerName = function() {
+  var name = ""; 
+  while (name === "" || name === null){
+    name = prompt("What is your robot's name?");
+  }
+}
 
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -131,6 +138,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    debugger; 
 
     // pick new enemy to fight based on the index of the enemyNames array
       var pickedEnemyObj = enemyInfo[i];
@@ -146,11 +154,10 @@ var startGame = function() {
      if (playerInfo.health > 0 && i < enemyInfo.length -1){
        var storeConfirm = window.confirm("The fight is over, visit the store?");
 
-       if (storeConfirm){
-      
-       shop(); 
+       if (storeConfirm) {
+             shop(); 
+       }
      }
-    }
    }
   // if player isn't alive, stop the game
     else {
